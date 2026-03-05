4511aa3803169b897f0b05fcc19da92f4a5d72ff
Guilherme Corrêa <guilherme_correa1879@hotmail.com>
2026-03-05 15:56:38 -0300

    Just needs API testing.

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -3,14 +3,15 @@ CREATE TABLE product
     id    BIGSERIAL PRIMARY KEY,
     name  VARCHAR(255)   NOT NULL,
     price DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
-        CONSTRAINT uq_product_name UNIQUE (name)
+    CONSTRAINT uq_product_name UNIQUE (name)
 );
 
 CREATE TABLE raw_material
 (
     id             BIGSERIAL PRIMARY KEY,
     name           VARCHAR(255)   NOT NULL,
-    stock_quantity DECIMAL(10, 3) NOT NULL DEFAULT 0 CHECK (stock_quantity >= 0)
+    stock_quantity DECIMAL(10, 3) NOT NULL DEFAULT 0 CHECK (stock_quantity >= 0),
+        CONSTRAINT uq_raw_material_name UNIQUE (name)
 );
 
 CREATE TABLE product_composition
@@ -20,7 +21,6 @@ CREATE TABLE product_composition
     raw_material_id   BIGINT         NOT NULL,
     required_quantity DECIMAL(10, 3) NOT NULL CHECK (required_quantity > 0),
 
-    -- Vai garantr que não tenha duplicidade no mesmo produto
     CONSTRAINT uq_product_raw_material UNIQUE (product_id, raw_material_id),
 
     CONSTRAINT fk_product FOREIGN KEY (product_id)
